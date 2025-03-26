32a16bb5f0ca8e659d7cdcd6310d143fa490dab7
Jebicott <yongchan0302@gmail.com>
2025-03-26 12:55:58 +0900

    Save Point System. R Respawn

---
+++ Assets/KYC/Scripts/Door.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Door : MonoBehaviour
+{
+    public bool forPlayer1; // Player1용 문인지 여부
+    private Vector3 spawnPointExtra;
+
+    void Start()
+    {
+        spawnPointExtra = new Vector3(0f, 0f, 3f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (forPlayer1)
+            {
+                // Player1이 통과하면 임시 스폰 포인트 설정
+                GameManager.Instance.SetTempPlayer1SpawnPoint(transform.position + spawnPointExtra);
+                GameManager.Instance.SetPlayer1Passed(true);
+                Debug.Log("player1 passed and set temp spawn point: " + (transform.position + spawnPointExtra));
+            }
+            else
+            {
+                // Player2가 통과하면 임시 스폰 포인트 설정
+                GameManager.Instance.SetTempPlayer2SpawnPoint(transform.position + spawnPointExtra);
+                GameManager.Instance.SetPlayer2Passed(true);
+                Debug.Log("player2 passed and set temp spawn point: " + (transform.position + spawnPointExtra));
+            }
+
+            // 두 플레이어가 모두 통과했는지 확인
+            if (GameManager.Instance.AreBothPlayersPassed())
+            {
+                Debug.Log("Both players passed");
+                GameManager.Instance.CommitSpawnPoints(); // 임시 스폰 포인트를 영구 적용
+                GameManager.Instance.PlusStageData();
+                GameManager.Instance.ShowStageData();
+                GameManager.Instance.ResetPassedFlags(); // 플래그 초기화
+            }
+        }
+    }
+
+    void Update()
+    {
+        // Update에서 추가 처리 불필요
+    }
+}
